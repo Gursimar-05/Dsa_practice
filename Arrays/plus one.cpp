121e2a62f4f9fb72a236a037b93344a7c7b10182
Gursimar_05 <gd4395@srmist.edu.in>
2026-06-11 19:14:33 +0530

    solved plus one

---
+++ Arrays/plus one.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+       int i=digits.size()-1;
+        if(digits[i]!=9){
+            digits[i]++;
+            return digits;
+        }
+        if(digits[i]==9){
+            digits[i]=0;
+            for(int j=i-1;j>=0;j--){
+                if(digits[j]!=9){
+                    digits[j]++;
+                    return digits;
+                }else{
+                    digits[j]=0;
+                }
+            }
+            digits.insert(digits.begin(),1);
+            return digits;
+
+        }
+        return digits;
+    }
+};
