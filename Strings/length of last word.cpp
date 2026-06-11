0ffc1cea8fd22bc9f38dc709d8195e7d5e02b366
Gursimar_05 <gd4395@srmist.edu.in>
2026-06-11 17:02:08 +0530

    solved length of last word

---
+++ Strings/length of last word.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int i=s.size()-1;
+        int count=0;
+       while(s[i] ==' '){
+         i--;
+       }
+        while(i>=0 && isalnum(s[i])){
+            i--;
+            count++;
+        }
+        return count;
+    }
+};
