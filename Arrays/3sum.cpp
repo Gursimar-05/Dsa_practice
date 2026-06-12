22e8ab1ae9af225a0835f7092af6edc44b467718
Gursimar_05 <gd4395@srmist.edu.in>
2026-06-12 18:19:26 +0530

    solved 3sum

---
+++ Arrays/3sum.cpp
@@ -0,0 +1,33 @@
+#include<algorithm>
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> result;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size()-1;i++){
+            if(i>0 && nums[i]==nums[i-1]){
+                continue;
+            }
+         int j=i+1;
+         int k=nums.size()-1;
+           while(j<k){
+                if(nums[i]+nums[j]+nums[k]==0){
+                result.push_back({nums[i],nums[j],nums[k]});
+                while(j<k && nums[j]==nums[j+1]){
+                     j++;
+                }
+                while(k>j && nums[k]==nums[k-1]){
+                    k--;
+                }
+                   j++;
+                   k--;
+            }else if(nums[i]+nums[j]+nums[k]>0){
+                k--;
+            }else{
+                j++;
+              }
+                }
+        }
+        return result;
+    }
+};
