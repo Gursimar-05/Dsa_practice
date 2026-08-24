7624a94689a6a9d30920aa10cfa4b9ca80f15ecb
Gursimar_05 <gd4395@srmist.edu.in>
2026-08-24 14:33:44 +0530

    solved find first and last occurence

---
+++ Arrays/first and last occurence.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+    int left=0;
+    int right=nums.size()-1;
+    int s=-1,e=-1;
+
+    while(left<=right){
+        int mid=left+(right-left)/2;
+        if(nums[mid]==target){
+            s=mid;
+            right=mid-1;
+        }
+        else if(nums[mid]<target){
+            left=mid+1;
+        }
+        else{
+            right=mid-1;
+        }
+    }
+    left=0;
+    right=nums.size()-1;
+    while(left<=right){
+        int mid=left+(right-left)/2;
+        if(nums[mid]==target){
+            e=mid;
+            left=mid+1;
+        }
+        else if(nums[mid]<target){
+            left=mid+1;
+        }
+        else{
+            right=mid-1;
+        }
+    }
+    return {s,e};
+}
+};
