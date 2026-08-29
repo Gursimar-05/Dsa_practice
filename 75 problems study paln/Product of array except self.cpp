ff7aa3b5e4510448d53247bf5c83298f422962ec
Gursimar_05 <gd4395@srmist.edu.in>
2026-08-29 21:18:04 +0530

    solved product of array except self

---
+++ 75 problems study paln/Product of array except self.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> ans(nums.size());
+        int n=nums.size();
+        ans[0]=1;
+        for(int i=1;i<n;i++){
+         ans[i]=nums[i-1]*ans[i-1];
+        }
+        int suffix=1;
+        for(int i=n-1;i>=0;i--){
+            ans[i]=suffix*ans[i];
+            suffix*=nums[i];
+        }
+        return ans;
+    }
+};
