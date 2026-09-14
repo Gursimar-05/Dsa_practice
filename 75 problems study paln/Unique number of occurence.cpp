c372063ddfefb2d0d84ec9612a727b8da708b6b4
Gursimar_05 <gd4395@srmist.edu.in>
2026-09-14 21:28:36 +0530

    solved unique number of occurence

---
+++ 75 problems study paln/Unique number of occurence.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int,int> mp;
+        for(int i=0;i<arr.size();i++){
+            mp[arr[i]]++;
+            
+        }
+        unordered_set<int> s;
+        for(auto i: mp){
+            if(s.count(i.second)){
+                return false;
+            }
+            s.insert(i.second);
+        }
+        return true;
+    }
+};
